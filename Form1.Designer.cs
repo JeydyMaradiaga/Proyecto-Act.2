44a296aa3e6889c1cab3c7e40782c75b3be84c39
JeydyMaradiaga <98502330+JeydyMaradiaga@users.noreply.github.com>
2022-02-21 22:34:01 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Act._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_registrar = new System.Windows.Forms.Button();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_edad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb_registro = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_registrar
+            // 
+            this.btn_registrar.Location = new System.Drawing.Point(271, 87);
+            this.btn_registrar.Name = "btn_registrar";
+            this.btn_registrar.Size = new System.Drawing.Size(87, 24);
+            this.btn_registrar.TabIndex = 0;
+            this.btn_registrar.Text = "Registrar";
+            this.btn_registrar.UseVisualStyleBackColor = true;
+            this.btn_registrar.Click += new System.EventHandler(this.btn_registrar_Click);
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(152, 33);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(105, 20);
+            this.txt_nombre.TabIndex = 1;
+            // 
+            // txt_edad
+            // 
+            this.txt_edad.Location = new System.Drawing.Point(483, 33);
+            this.txt_edad.Name = "txt_edad";
+            this.txt_edad.Size = new System.Drawing.Size(105, 20);
+            this.txt_edad.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese si nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(335, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingrese su edad";
+            // 
+            // cb_registro
+            // 
+            this.cb_registro.FormattingEnabled = true;
+            this.cb_registro.Location = new System.Drawing.Point(256, 196);
+            this.cb_registro.Name = "cb_registro";
+            this.cb_registro.Size = new System.Drawing.Size(142, 21);
+            this.cb_registro.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(268, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Estudiantes registrados";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cb_registro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_edad);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.btn_registrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_registrar;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_edad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_registro;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Act._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_registrar_Click(object sender, EventArgs e)
+        {
+            //Declaración de arreglos
+            string[] nombres = new string [1];
+            int[] edad = new int[1 ];
+
+            //Se llena el arreglo de nombres
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                nombres[i] = txt_nombre.Text;
+            }
+
+            // Se llena el arreglo de edad
+            for (int i = 0; i < edad.Length; i++)
+            {
+                edad[i] = Convert.ToInt16(txt_edad.Text);
+            }
+
+            //Muestra los datos en el ComboBox
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                cb_registro.Items.Add("Nombre: " + (nombres[i]) + " Edad: " + (edad[i]));
+            }
+
+            //se borra los textBox para poder ingresar nuevos datos
+            txt_edad.Clear();
+            txt_nombre.Clear();
+
+
+        }
+
+
+    }
+}
